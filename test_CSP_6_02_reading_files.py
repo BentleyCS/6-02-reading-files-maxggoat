4aed5c0fdac38b8acc6cd302e5c4fd2ee20e4133
maxggoat <mgorodnichenko28@bentleyschool.org>
2026-03-03 20:58:35 -0800

    Add files via upload

--- test_CSP_6_02_reading_files.py
+++ test_CSP_6_02_reading_files.py
@@ -7,7 +7,7 @@ def test_toString():
     assert toString("sample2.txt")=="Here is the text\ni am another line"
 
 def test_longestLine():
-    assert longestLine("sample2.txt") == "Here is the text\n"
+    assert longestLine("sample2.txt") == "i am another line"
 
 def test_toBinary():
     assert toBinary("sample3.txt") == ['01101001', '00101010', '1010']
